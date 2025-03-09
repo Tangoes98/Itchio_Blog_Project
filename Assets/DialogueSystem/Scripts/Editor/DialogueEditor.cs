3752c68f1cc988bfdd01b6103119eeba284e57a2
Tangoes98 <101127210+Tangoes98@users.noreply.github.com>
2025-03-09 19:51:54 -0400

    Add double click dialogue so to open dialogue window

--- Assets/DialogueSystem/Scripts/Editor/DialogueEditor.cs
+++ Assets/DialogueSystem/Scripts/Editor/DialogueEditor.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
+using UnityEditor.Callbacks;
 using UnityEngine;
 
 namespace DialogueSystem.Editor
 {
     public class DialogueEditor : EditorWindow
     {
+        //* To popup a Dialogue Editor window in Unity Editor
         [MenuItem("Window/Dialogue Editor")]
         public static void ShowWindow()
         {
@@ -21,6 +23,26 @@ namespace DialogueSystem.Editor
             {
                 Debug.Log("Creating new dialogue");
             }
+        }
+
+        //* To open a Dialogue Editor window when a Dialogue asset is double clicked
+        [OnOpenAsset(1)]
+        public static bool OnOpenDialogueSoAsset(int instanceID, int line)
+        {
+            var dialogueSo = EditorUtility.InstanceIDToObject(instanceID) as So_Dialogue;
+            if (dialogueSo != null)
+            {
+                ShowWindow();
+                Debug.Log("Opening dialogue: " + dialogueSo.name);
+                return true;
+            }
+            // if (Selection.activeObject.GetType() == typeof(So_Dialogue))
+            // {
+            //     So_Dialogue dialogue = Selection.activeObject as So_Dialogue;
+            //     Debug.Log("Opening dialogue: " + dialogue.name);
+            //     return true;
+            // }
+            return false;
         }
     }
 }
